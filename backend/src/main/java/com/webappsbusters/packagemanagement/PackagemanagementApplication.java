ee6069056dfa565ef2802e53fc249c19f518cbb6
TheFlyingKeyboard <theflyingkeyboard2@gmail.com>
2020-11-22 09:20:06 +0100

    Backend Init

---
+++ backend/src/main/java/com/webappsbusters/packagemanagement/PackagemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.webappsbusters.packagemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackagemanagementApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/webappsbusters/packagemanagement/PackagemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webappsbusters.packagemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackagemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
