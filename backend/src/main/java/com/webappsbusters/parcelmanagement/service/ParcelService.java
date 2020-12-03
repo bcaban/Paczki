8c502a6b0e32ad4e7d9ed2aa66a7088047369496
bcaban <b.caban1996@gmail.com>
2020-12-03 19:58:32 +0100

    Implementation of retrieving parcel data using id (PAC-28 and PAC-29)

---
+++ backend/src/main/java/com/webappsbusters/parcelmanagement/service/ParcelService.java
@@ -0,0 +1,24 @@
+package com.webappsbusters.parcelmanagement.service;
+
+import com.webappsbusters.parcelmanagement.domain.Parcel;
+import com.webappsbusters.parcelmanagement.repository.ParcelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class ParcelService {
+    private final ParcelRepository parcelRepository;
+
+    @Autowired
+    public ParcelService(ParcelRepository parcelRepository) {
+        this.parcelRepository = parcelRepository;
+    }
+
+    public Optional<Parcel> getParcelById(final String id) {
+        return parcelRepository.findById(id);
+    }
+}
